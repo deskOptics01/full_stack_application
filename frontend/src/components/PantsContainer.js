81e1d83f847c2341d08f5262ffe940662b3d64fb
deskOptics01 <business1stnever2nd@gmail.com>
2022-08-01 19:08:19 -0700

    added first containers(pants, shirts), inside components.js folder

---
+++ frontend/src/components/PantsContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PantsContainer() {
+  return (
+    <div>
+        <h2># of Pants</h2>
+        <button>Buy Pants</button>
+    </div>
+  )
+}
+
+export default PantsContainer
---
+++ frontend/src/components/ShirtContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ShirtContainer() {
+  return (
+    <div>
+        <h2>Number of shirts</h2>
+        <button>Buy Shirts</button>
+    </div>
+  )
+}
+
+export default ShirtContainer
